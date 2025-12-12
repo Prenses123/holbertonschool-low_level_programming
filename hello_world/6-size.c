778138933024cb7e5fcb67b907c75d37e92a1e63
Your Name <you@example.com>
2025-12-12 17:13:10 +0000

    Add

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	printf("Size of a char: %d 1 byte(s)\n", sizeof(char));
+	printf("Size of an int: %d 4 byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d 4 byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d 8 byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d  4 byte(s)\n", sizeof(float));
+	return (0);
+}	
