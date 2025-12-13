c1ec374a8afbe5b42f786d28faefed3327ae999c
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2025-12-13 12:34:37 +0400

    Update 2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -5,14 +5,12 @@
  * Return: 0 if successful
  */
 int main(void)
-
-{ 
+{
     char a = 'a';
-    while(a <= 'z') {
+    while (a <= 'z'){
         putchar(a);
         a++;
     }
     putchar('\n');
-    
     return 0;
 }
