2b3a838c5296727deba1118fa68effc585fd2f22
Your Name <you@example.com>
2025-12-13 14:01:01 +0000

    add

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 if successful
+ */
+void print_alphabet(void);
+
+int main(void)
+{
+	print_alphabet(void);
+    	return (0);
+}
+void print_alphabet(void)
+{
+	char c;
+
+	a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+}
