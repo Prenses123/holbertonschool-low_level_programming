714c1bf7dbc3e1288ac99344a594a689dbdd7be9
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2025-12-13 18:23:19 +0400

    Update _putchar.c

--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -1,6 +1,13 @@
 #include <unistd.h>
 #include "main.h"
 
+/**
+ * _putchar - prints a character to stdout
+ * @c: character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned.
+ */
 int _putchar(char c)
 {
     return (write(1, &c, 1));
