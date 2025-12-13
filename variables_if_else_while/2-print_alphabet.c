2512469e6b9c917663e36e86ba3572e78d7ff27a
Your Name <you@example.com>
2025-12-13 08:26:31 +0000

    add

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 if successful
+ */
+int main(void)
+
+{ 
+    char a = 'a';
+    while(a <= 'z') {
+        putchar(a);
+        a++;
+    }       
+
+    return 0;
+}	
