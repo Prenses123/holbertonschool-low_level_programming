f2bd4f5d67ba995fd71497df84b6ca3149d96707
Your Name <you@example.com>
2025-12-13 09:33:21 +0000

    add

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char z;
+
+	z = 'z';
+
+	while (z >= 'a')
+	{
+		putchar(a);
+		a--;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
