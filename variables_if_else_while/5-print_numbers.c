3d2ac6b6b50e87846680332f916eb0f16b07961b
Your Name <you@example.com>
2025-12-13 09:18:42 +0000

    aa 5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
