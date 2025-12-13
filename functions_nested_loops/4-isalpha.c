d7a33259a09e10bb766a255e238f1b9491853956
Your Name <you@example.com>
2025-12-13 16:20:27 +0000

    V

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for lowercase and uppercase character
+ * @c: the character to check
+ *
+ * Return: 1 if c is lowercase and uppercase, 0 otherwise
+ */
+int _isalpha(int c)
+
+{
+	if (c >= 'A' && c < 'z')
+		return (1);
+	return (0);		
+} 
