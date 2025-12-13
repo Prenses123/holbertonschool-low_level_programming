0c2fe2c4dac29191eaaf61d3a10391410844cc06
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2025-12-13 18:33:43 +0400

    Update 1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,23 +1,17 @@
-#include <stdio.h>
+#include "main.h"
 
-/**
- * main - Entry point
- * Return: 0 if successful
- */
-void print_alphabet(void);
-
-int main(void)
-{
-	print_alphabet();
-    	return (0);
-}
-
-void _putchar(c);
-print_alphabet()
+void print_alphabet(void)
 {
     char c;
 
     for (c = 'a'; c <= 'z'; c++)
         _putchar(c);
     _putchar('\n');
+}
+#include "main.h"
+
+int main(void)
+{
+    print_alphabet();
+    return (0);
 }
