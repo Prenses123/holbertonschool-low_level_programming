20c9943e9fa0c7208276c75511242283f5823e51
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2025-12-13 20:22:04 +0400

    Update main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -5,7 +5,8 @@ int _putchar(char c);         /* _putchar funksiyası */
 void print_alphabet(void);    /* print_alphabet funksiyası */
 void print_alphabet_x10(void);         /* print_alphabet_x10 funksiyasi */
 int _islower(int c);          /*int _islower(int c) funksiyasi*/
-
+int _isalpha(int c);          /* int _isalpha(int c) funksiyasi*/
 
 
 #endif /* MAIN_H */
+
