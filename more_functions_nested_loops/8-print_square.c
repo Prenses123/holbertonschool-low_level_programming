35a91acd4d17201f9c75f12763832dae20dd231b
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2025-12-14 23:41:42 +0400

    Update 8-print_square.c

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -8,14 +8,18 @@
  */
 void print_square(int size)
 {
-	int i;
+	int i, j;
 
 	if (size <= 0)
 	{
 		_putchar('\n');
 		return;
 	}
-	for (i = 0; i < size;i++)
-		_putchar('#');
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar('#');
+	}
 	_putchar('\n');
+		
 }
