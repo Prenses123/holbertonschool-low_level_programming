594754b0319b19314496534bcde1f8637d182d98
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2025-12-15 23:46:50 +0400

    Update 2-strlen_recursion.c

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -4,14 +4,16 @@
  * _strlen_recursion - Returns the length of a string
  * @s: Pointer to the string
  *
- * Return: Length of the string
+ * Description: uses recursion to count characters until '\0'
+ *
+ * Return: length of the string
  */
 int _strlen_recursion(char *s)
 {
-    // Base case: if we reach the null terminator '\0', the length is 0
+    /* Base case: if we reach the null terminator '\0', return 0 */
     if (*s == '\0')
         return 0;
 
-    // Recursive case: 1 (current character) + length of the rest of the string
+    /* Recursive case: 1 + length of the rest of the string */
     return 1 + _strlen_recursion(s + 1);
 }
