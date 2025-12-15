5fbe8cf6234f5bf202bc6235f400149239ed491a
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2025-12-15 23:34:18 +0400

    Update 0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,7 +1,14 @@
 #include <unistd.h>
 
+/* prototype for _putchar */
 int _putchar(char c);
 
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: string to print
+ *
+ * Description: uses recursion to print each character of the string
+ */
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
