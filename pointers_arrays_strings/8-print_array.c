54588e4be1ac61fdea9bb92e08541b3c8752ff07
Your Name <you@example.com>
2025-12-17 13:20:52 +0000

    add

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an integer array
+ * @a: pointer to the first element of the array
+ * @n: number of elements to print
+ */
+void print_array(int *a, int n)
+{
+	for (i = 0;i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != (n - 1))
+			printf(", ");
+	}
+	printf("\n");
+}	
