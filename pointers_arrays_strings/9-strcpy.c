9aa1f3d2956d68958eb2bd5bc98d11ac33c944f2
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2025-12-17 17:47:28 +0400

    Update 9-strcpy.c

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -8,14 +8,15 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	char *ptr = dest;
+    char *ptr = dest;
 
-	while (*src != '\0')
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
+    while (*src != '\0')
+    {
+        *dest = *src;
+        dest++;
+        src++;
+    }
 
-	*dest = '\0';
-	return (ptr);
+    *dest = '\0';
+    return ptr;
+}
