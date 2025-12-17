59dcd307d2de5e59eb1ac54e56f9934b9e583a56
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2025-12-17 17:48:08 +0400

    Update 9-strcpy.c

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -4,19 +4,20 @@
  * _strcpy - copies the string pointed to by src to dest
  * @dest: destination buffer
  * @src: source string
+ *
  * Return: pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
-    char *ptr = dest;
+	char *ptr = dest;
 
-    while (*src != '\0')
-    {
-        *dest = *src;
-        dest++;
-        src++;
-    }
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
 
-    *dest = '\0';
-    return ptr;
+	*dest = '\0';
+	return (ptr);
 }
