30395cfdf1aed1858b87b3a54e5715c862ea62a9
Your Name <you@example.com>
2025-12-18 09:23:20 +0000

    add

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * _atoi - converts a string to an integer
@@ -9,21 +10,35 @@
 int _atoi(char *s)
 {
 	int sign = 1;
-	int result = 0;
+	long result = 0;
 	int started = 0;
 
-	while (*s != '\0')
+	while (*s)
 	{
-		if (*s == '-')
+		if (*s == '-' && !started)
 			sign *= -1;
 		else if (*s >= '0' && *s <= '9')
 		{
 			started = 1;
+
+			/* overflow check */
+			if (result > (LONG_MAX - (*s - '0')) / 10)
+				return (sign == 1 ? INT_MAX : INT_MIN);
+
 			result = result * 10 + (*s - '0');
 		}
 		else if (started)
 			break;
+
 		s++;
 	}
-	return (sign * result);
+
+	result *= sign;
+
+	if (result > INT_MAX)
+		return (INT_MAX);
+	if (result < INT_MIN)
+		return (INT_MIN);
+
+	return ((int)result);
 }
