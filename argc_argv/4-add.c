78796ca377fbe347b4ae721cfb79ebc6a987cbbf
Your Name <you@example.com>
2025-12-19 14:07:57 +0000

    add

---
+++ argc_argv/4-add.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * is_number - checks if a string contains only digits
+ * @s: string to check
+ *
+ * Return: 1 if string is a valid positive number, 0 otherwise
+ */
+int is_number(char *s)
+{
+    int i = 0;
+
+    if (s[0] == '\0') /* boş string yoxla */
+        return 0;
+
+    while (s[i])
+    {
+        if (!isdigit(s[i]))
+            return 0;
+        i++;
+    }
+    return 1;
+}
+
+/**
+ * main - adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+    int i, sum = 0;
+
+    if (argc == 1)  /* heç bir arqument yoxdursa */
+    {
+        printf("0\n");
+        return 0;
+    }
+
+    for (i = 1; i < argc; i++)
+    {
+        if (!is_number(argv[i]))  /* simvol yoxlaması */
+        {
+            printf("Error\n");
+            return 1;
+        }
+        sum += atoi(argv[i]);
+    }
+
+    printf("%d\n", sum);
+    return 0;
+}
