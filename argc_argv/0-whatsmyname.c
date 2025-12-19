9c2a8a0cc8207fe9551e82bcd563168aa1bf82cd
Your Name <you@example.com>
2025-12-19 13:01:10 +0000

    add

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: argument count (not used)
+ * @argv: array of argument strings
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", arg[0]);
+	return (0);
+}
