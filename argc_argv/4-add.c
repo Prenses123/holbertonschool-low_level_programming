706fd4ffcbf1f83d320b75c26502b2acc9d401f2
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2025-12-19 18:10:17 +0400

    Update 4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -29,4 +29,28 @@ int is_number(char *s)
  * @argc: argument count
  * @argv: argument vector
  *
- * Return: 0 on success, 1 on er*
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+  int i, sum = 0;
+
+  if (argc == 1)
+  {
+    printf("0\n");
+    return (0);
+  }
+
+  for (i = 1; i < argc; i++)
+  {
+    if (!is_number(argv[i]))
+    {
+      printf("Error\n");
+      return (1);
+    }
+    sum += atoi(argv[i]);
+  }
+
+  printf("%d\n", sum);
+  return (0);
+}
