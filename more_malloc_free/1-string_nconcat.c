a086280beb1386d22ca7056d764d6d024d5b4ef8
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2025-12-28 19:24:17 +0400

    Update 1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -30,7 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	for (i = 0; i < len1; i++)
 		new[i] = s1[i];
-	for (j = 0; j < len2; j++)
+	for (j = 0; j < n; j++)
 		new[i + j] = s2[j];
 
  	new[i + j] = '\0';
