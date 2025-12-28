4627fe9e03c3f2d7a1811ae8ad2014641af2cc38
Your Name <you@example.com>
2025-12-28 15:18:52 +0000

    aff

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,16 +12,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 
 	char *new;
-	int i, j;
-	int len1 = 0, len2 = 0;
+	unsigned int i, j;
+	unsigned int len1 = 0, len2 = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (s1 != '\0')
+	while (s1[len1] != '\0')
 		len1++;
-	while (s2 != '\0')
+	while (s2[¶len2] != '\0')
 		len2++;
 	if (n >= len2)
 		n = len2;
