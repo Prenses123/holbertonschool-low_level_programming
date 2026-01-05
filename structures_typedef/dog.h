9e82bfc799c23ac39c5aa65464c88f64f310ee5a
Your Name <you@example.com>
2026-01-05 10:23:11 +0000

    add

---
+++ structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct dog = struct 
+ */
+struct dog
+{
+	char* name;
+	float age;
+	char* owner;
+};
+int main()
+{
+	struct dog x;
+	x.name = "Poppy";
+	x.age = 3.5;
+	x.owner = "Bob";
+	printf("%s\n", x.name);
+	printf("%f\n", x.age);
+	printf("%s\n", x.name);
+	return (0);
+}
