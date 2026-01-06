b42e02320f9191eb3eac9100ee916ca169610325
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2026-01-06 14:24:17 +0400

    Update 0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,6 +2,8 @@
 #include <string.h>
 /*
  * print_name = function that prints name
+ * @name: pointer to the name to print
+ * @f: pointer to a function that takes a char* and returns void
  */
 void print_name(char *name, void (*f)(char *))
 {
