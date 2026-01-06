f6db4d937d0b7f50c58dd2cf40f3335ea73a0c3a
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2026-01-06 14:43:57 +0400

    Update 1-array_iterator.c

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -4,6 +4,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	if (array == NULL || action == NULL)
 		return;
-	for ( size_t i = 0; i < size; i++)
+	size_t i;
+	for (i = 0; i < size; i++)
 		action(array[i]);
 }		
