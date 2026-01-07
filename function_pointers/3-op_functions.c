eb4f946e7a5f0fe25ca55cdcb0bdc298e5d4715b
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2026-01-07 15:24:46 +0400

    Update 3-op_functions.c

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,21 +1,56 @@
 #include "3-calc.h"
+
+/**
+ * op_add - toplama əməliyyatı
+ * @a: birinci ədəd
+ * @b: ikinci ədəd
+ * Return: a + b
+ */
 int op_add(int a, int b)
 {
-	return (a + b);
+    return (a + b);
 }
+
+/**
+ * op_sub - çıxma əməliyyatı
+ * @a: birinci ədəd
+ * @b: ikinci ədəd
+ * Return: a - b
+ */
 int op_sub(int a, int b)
 {
-	return (a - b);
+    return (a - b);
 }
+
+/**
+ * op_mul - vurma əməliyyatı
+ * @a: birinci ədəd
+ * @b: ikinci ədəd
+ * Return: a * b
+ */
 int op_mul(int a, int b)
 {
-	return (a * b);
+    return (a * b);
 }
+
+/**
+ * op_div - bölmə əməliyyatı
+ * @a: birinci ədəd
+ * @b: ikinci ədəd
+ * Return: a / b
+ */
 int op_div(int a, int b)
 {
-	return (a / b);
+    return (a / b);
 }
+
+/**
+ * op_mod - modul əməliyyatı
+ * @a: birinci ədəd
+ * @b: ikinci ədəd
+ * Return: a % b
+ */
 int op_mod(int a, int b)
 {
-	return (a % b);
+    return (a % b);
 }
