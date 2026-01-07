9a286d78069cd7818c8faa7f0e7c12ed15d4b5ff
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2026-01-07 15:29:55 +0400

    Update 3-get_op_func.c

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -5,26 +5,26 @@
 /**
  * get_op_func - operatora uyğun funksiyanı qaytarır
  * @s: operator simvolu
- * Return: pointer operator funksiyasına
+ * Return: pointer operator funksiyasına, NULL əgər operator
+ * uyğun deyilsə
  */
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-    while (ops[i].op)
-    {
-        if (strcmp(s, ops[i].op) == 0)
-            return (ops[i].f);
-        i++;
-    }
-
-    return (NULL);
+	while (ops[i].op)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
