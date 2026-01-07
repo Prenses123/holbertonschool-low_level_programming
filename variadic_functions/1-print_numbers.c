51ff1b33c334ffcc42f253399ca07a108cf98b3c
Your Name <you@example.com>
2026-01-07 17:41:17 +0000

    add

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_numbers - function that prints numbers
+ * @n: number of arguments
+ * Return:noting
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list args;
+
+	va_start(args,n);
+	if (n <= 0)
+		return;
+	for (i = 0; i < n; i++)
+	{
+		int number = va_arg(args,int);
+
+		printf("%d", number);
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
+}
