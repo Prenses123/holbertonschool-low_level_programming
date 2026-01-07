9bbb5b75c7e5580a82fb26a96ef5a1a13f921c6f
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2026-01-07 15:05:42 +0400

    Update 3-calc.h

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,20 +1,22 @@
-#ifndef CALC.H
-#define CALC.H
+#ifndef CALC_H
+#define CALC_H
+
 /**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
+ * struct op - operator və ona uyğun funksiya
+ * @op: operator simvolu
+ * @f: operator funksiyasına pointer
  */
 typedef struct op
 {
     char *op;
     int (*f)(int a, int b);
 } op_t;
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
 
-#endif
+#endif /* CALC_H */
