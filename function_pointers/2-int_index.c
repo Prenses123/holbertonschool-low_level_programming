b220ecfb1a6bff29f25517018eb232e9a1c92a9e
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2026-01-07 13:51:44 +0400

    Update 2-int_index.c

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -12,10 +12,13 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
+
     if (array == NULL || cmp == NULL)
 		return (-1);
+
 	if (size <= 0)
 		return (-1);
+
 	for (i = 0; i < size; i++)
 	{
 	       if (cmp(array[i]) != 0)
@@ -23,5 +26,6 @@ int int_index(int *array, int size, int (*cmp)(int))
 			return (i);
 		   }
 	}
+
 	return (-1);
 }
