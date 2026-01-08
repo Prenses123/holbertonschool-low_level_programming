5fd8dd8528aee2183c5c6935c2866e7cad36c099
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2026-01-08 12:57:53 +0400

    Update 2-print_strings.c

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -16,7 +16,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 	if (n <= 0)
+	{
+		printf("\n");
 		return;
+	}
 	for (i = 0; i < n; i++)
 	{
 		char *x = va_arg(args, char *);
