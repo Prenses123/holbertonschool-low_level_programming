ea96f3096c54241bbb0cbba8ee2d12bdbfb711fb
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2026-01-08 12:50:31 +0400

    Update 2-print_strings.c

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -19,12 +19,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		return;
 	for (i = 0; i < n; i++)
 	{
-		char x = va_arg(args, char);
+		char* x = va_arg(args, char);
 
 		if (x == NULL)
 			printf("(nil)");
 		if (x != NULL)
-			printf("%s", x)
+			printf("%s", x);
 		else if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
