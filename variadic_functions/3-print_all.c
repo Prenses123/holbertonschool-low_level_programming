092b04d4eeaeaf3c2bfcf496d335085b6d04c16f
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2026-01-08 23:00:51 +0400

    Update 3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -11,32 +11,47 @@ void print_all(const char * const format, ...)
 {
 	va_list ap;
 	int i = 0;
-	char *sep = "", *s;
+	char *sep = "";
+	char *s;
 
 	va_start(ap, format);
+
 	if (!format)
 	{
 		printf("\n");
 		va_end(ap);
 		return;
 	}
+
 	while (format[i])
 	{
 		switch (format[i])
 		{
-		case 'c': printf("%s%c", sep, va_arg(ap, int)); sep = ", "; break;
-		case 'i': printf("%s%d", sep, va_arg(ap, int)); sep = ", "; break;
-		case 'f': printf("%s%f", sep, va_arg(ap, double)); sep = ", "; break;
+		case 'c':
+			printf("%s%c", sep, va_arg(ap, int));
+			sep = ", ";
+			break;
+		case 'i':
+			printf("%s%d", sep, va_arg(ap, int));
+			sep = ", ";
+			break;
+		case 'f':
+			printf("%s%f", sep, va_arg(ap, double));
+			sep = ", ";
+			break;
 		case 's':
 			s = va_arg(ap, char *);
-			if (!s) s = "(nil)";
+			if (!s)
+				s = "(nil)";
 			printf("%s%s", sep, s);
 			sep = ", ";
 			break;
-		default: break;
+		default:
+			break;
 		}
 		i++;
 	}
+
 	printf("\n");
 	va_end(ap);
 }
