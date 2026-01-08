d744551a584437362251c2311310f7501f05a7e3
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2026-01-08 14:07:24 +0400

    Update 3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -26,25 +26,25 @@ void print_all(const char * const format, ...)
 			case 'c':
 			{
 				int c = va_arg(args, int);
-                printf("%c\n", c);
+                printf("%c,", c);
 				break;
 			}
 			case 'i':
 			{
 				int i = va_arg(args, int);
-				printf("%d", i);
+				printf("%d,", i);
 				break;
 			}
 			case 'f':
 			{
 				double f = va_arg(args, double);
-				printf("%f", f);
+				printf("%f,", f);
 				break;
 			}
 			case 's':
 			{
 				char *s = va_arg(args, char *);
-				printf("%s\n", s ? s : "(nil)");
+				printf("%s,", s ? s : "(nil)");
 				break;
 			}
 			default:
