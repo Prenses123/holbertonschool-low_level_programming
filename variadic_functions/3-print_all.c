e7ffb49850c83a52e306b8bad2c6019a4726060d
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2026-01-08 22:30:22 +0400

    Update 3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,69 +1,48 @@
 #include <stdarg.h>
-#include <stdlib.h>
 #include <stdio.h>
 #include "variadic_functions.h"
+
 /**
- * print_all - function that prints all
- * @format: format of variables
- * Return: nothing
+ * print_all - prints anything
+ * @format: list of types of arguments
  */
 void print_all(const char * const format, ...)
 {
-	int i = 0;
-
+	int i;
+	int printed;
 	va_list args;
 
+	i = 0;
+	printed = 0;
 	va_start(args, format);
+
 	if (format == NULL)
 	{
 		printf("\n");
+		va_end(args);
 		return;
 	}
 
 	while (format[i])
 	{
-		int k = 0;
-
-		switch (format[i])
+		if (format[i] == 'c')
+			printed = printf("%c", va_arg(args, int));
+		if (format[i] == 'i')
+			printed = printf("%d", va_arg(args, int));
+		if (format[i] == 'f')
+			printed = printf("%f", va_arg(args, double));
+		if (format[i] == 's')
 		{
-			case 'c':
-			{
-				int c = va_arg(args, int);
-				printf("%c", c);
-				k = 1;
-				break;
-			}
-			case 'i':
-			{
-				int i = va_arg(args, int);
+			char *s = va_arg(args, char *);
 
-				printf("%d", i);
-				k = 1;
-				break;
-			}
-			case 'f':
-			{
-				double f = va_arg(args, double);
-
-				printf("%f", f);
-				k = 1;
-				break;
-			}
-			case 's':
-			{
-				char *s = va_arg(args, char *);
-
-				printf("%s", s ? s : "(nil)");
-				k = 1;
-				break;
-			}
-			default:
-				break;
+			printed = printf("%s", s ? s : "(nil)");
 		}
-		if (format[i + 1] && k == 1)
+		if (format[i + 1] && printed)
 			printf(", ");
+		printed = 0;
 		i++;
 	}
+
 	printf("\n");
 	va_end(args);
 }
