d23b6d53eae9308d6cd9f437f3e057a307d52576
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2026-01-13 16:51:33 +0400

    Update 0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,19 +8,19 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
-    const list_t *ptr = h;
+	size_t count = 0;
+	const list_t *ptr = h;
 
-    while (ptr != NULL)
-    {
-        if (ptr->str != NULL)
-            printf("[%u] %s\n", ptr->len, ptr->str);
-        else
-            printf("[0] (nil)\n");
+	while (ptr != NULL)
+	{
+		if (ptr->str != NULL)
+			printf("[%u] %s\n", ptr->len, ptr->str);
+		else
+			printf("[0] (nil)\n");
 
-        count++;
-        ptr = ptr->next;
-    }
+		count++;
+		ptr = ptr->next;
+	}
 
-    return count;
+	return (count);
 }
