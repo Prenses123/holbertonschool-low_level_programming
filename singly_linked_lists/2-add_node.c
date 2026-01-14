4a3cd37b9de786789fff804bb9577ce3f469e124
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2026-01-14 14:23:48 +0400

    Update 2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,27 +1,35 @@
 #include "lists.h"
-#include <stdio.h>
-#include <string.h> 
 #include <stdlib.h>
+#include <string.h>
+
 /**
- * add_node - function
- * @head: jddjj
- * @str:data
- * Return:the address of new element
+ * add_node - adds a new node at the beginning of a list
+ * @head: pointer to the head of the list
+ * @str: string to add
+ *
+ * Return: address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *ptr = malloc(sizeof(list_t));
-	
+	list_t *ptr;
+
+	if (head == NULL || str == NULL)
+		return (NULL);
+
+	ptr = malloc(sizeof(list_t));
 	if (ptr == NULL)
 		return (NULL);
-	else
-	{
-		ptr->str = strdup(str);
-		ptr->next = NULL;
 
-		ptr->next = head;
-		head = ptr;
-		while (ptr != NULL)
-			ptr = ptr->next;
-		return (head);
+	ptr->str = strdup(str);
+	if (ptr->str == NULL)
+	{
+		free(ptr);
+		return (NULL);
 	}
+
+	ptr->len = strlen(str);
+	ptr->next = *head;
+	*head = ptr;
+
+	return (ptr);
+}
