afc6bcb0f7739d0adbf990e8e797cfb7262e1380
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2026-01-15 13:50:26 +0400

    Update 3-add_node_end.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -18,7 +18,6 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (head == NULL || str == NULL)
 		return (NULL);
 
-	temp = malloc(sizeof(list_t));
 	if (temp == NULL)
 		return (NULL);
 	temp->str = strdup(str);
