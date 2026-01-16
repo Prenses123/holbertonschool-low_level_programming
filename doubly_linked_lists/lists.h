9ced94cfe3f3ea18478aec2a50d48c084828b4bd
Your Name <you@example.com>
2026-01-16 09:48:10 +0000

    add lists.h

---
+++ doubly_linked_lists/lists.h
@@ -0,0 +1,19 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
+/**
+ * struct list_s - singly linked list node
+ * @str: string - malloc'ed string
+ * @len: length of the string
+ * @next: points to the next node
+ */
+typedef struct list_s
+{
+	struct list_s prev;
+	char *str;
+	struct list_s *next;
+} list_t;
+size_t print_dlistint(const dlistint_t *h);
+
+#endif /* LISTS_H */
