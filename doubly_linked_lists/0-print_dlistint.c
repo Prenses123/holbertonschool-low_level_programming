ad86081f864f9006f3ee7714a6454e10fc30a292
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2026-01-16 13:58:43 +0400

    Update 0-print_dlistint.c

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -11,7 +11,7 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t count = 0;
 	const dlistint_t *ptr = h;
 
-	while(ptr != NULL)
+	while (ptr != NULL)
 	{
 		printf("%d\n", ptr->n);
 		ptr = ptr->next;
