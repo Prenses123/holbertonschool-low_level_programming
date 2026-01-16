5dc1fae861f70935a33baedeb84271d00e32a8f8
Your Name <you@example.com>
2026-01-16 10:20:15 +0000

    add

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * add_dnodeint - funbction
+ * @gead - head of list
+ * @n:data
+ * Return:number of nodes
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *ptr;
+
+	if (head == NULL)
+		return (NULL);
+	ptr = malloc(sizeof(dlistint_t));
+	if (ptr == NULL)
+		return (NULL);
+	ptr->prev = NULL;
+	ptr->n = n;
+	ptr->next = NULL;
+	ptr->next = *head;
+	(*head)->prev = temp;
+	*head = ptr;
+	return (ptr);
+
