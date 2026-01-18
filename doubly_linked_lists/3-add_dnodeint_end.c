1c798292de87973901a84a65f22c70397f8bff47
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2026-01-18 13:11:26 +0400

    Update 3-add_dnodeint_end.c

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -19,11 +19,12 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	temp->prev = NULL;
 	temp->n = n;
 	temp->next = NULL;
+	ptr = *head
 	while (ptr->next != NULL)
 		ptr = ptr->next;
 	temp->prev = *head;
-	if(*head != NULL)
-		(*head)->next = temp;
-	*head = temp;
+	if(*ptr != NULL)
+		ptr->next = temp;
+	ptr = temp;
 	return (temp);
 }
