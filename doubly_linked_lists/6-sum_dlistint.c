108464c50b20632a17897168477420d2877be640
Your Name <you@example.com>
2026-01-18 16:28:10 +0000

    add

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * sum_dlistint - function
+ * @head: head
+ * Return:eturn sums
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *ptr;
+	int i = 0, sum = 0;
+
+	if (head == NULL)
+		return (0);
+	ptr = head;
+	while (ptr != NULL)
+	{
+		ptr = ptr->next;
+		sum += ptr->n;
+	}
+	return (sum)
+}
