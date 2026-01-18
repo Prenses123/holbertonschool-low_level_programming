4108f8686af9879d911c09e4c50fca6dd350b486
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2026-01-18 21:34:49 +0400

    Update 8-delete_dnodeint.c

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -9,39 +9,38 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *ptr;
-    unsigned int i = 0;
+	dlistint_t *ptr;
+	unsigned int i = 0;
 
-    if (head == NULL || *head == NULL)
-        return (-1);
+	if (head == NULL || *head == NULL)
+		return (-1);
 
-    ptr = *head;
+	ptr = *head;
 
-    /* delete head node */
-    if (index == 0)
-    {
-        *head = ptr->next;
-        if (*head != NULL)
-            (*head)->prev = NULL;
-        free(ptr);
-        return (1);
-    }
+	if (index == 0)
+	{
+		*head = ptr->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(ptr);
+		return (1);
+	}
 
-    while (ptr != NULL && i < index)
-    {
-        ptr = ptr->next;
-        i++;
-    }
+	while (ptr != NULL && i < index)
+	{
+		ptr = ptr->next;
+		i++;
+	}
 
-    if (ptr == NULL)
-        return (-1);
+	if (ptr == NULL)
+		return (-1);
 
-    if (ptr->prev != NULL)
-        ptr->prev->next = ptr->next;
+	if (ptr->prev != NULL)
+		ptr->prev->next = ptr->next;
 
-    if (ptr->next != NULL)
-        ptr->next->prev = ptr->prev;
+	if (ptr->next != NULL)
+		ptr->next->prev = ptr->prev;
 
-    free(ptr);
-    return (1);
+	free(ptr);
+	return (1);
 }
