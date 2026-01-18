e6e3b847779a81434fc388cc0199bd68298412c2
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2026-01-18 14:25:59 +0400

    Update 5-get_dnodeint.c

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -11,9 +11,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {       
     dlistint_t *ptr;
     unsigned int i = 0;
-        
-    if (head == NULL)
-        return (NULL);
+
 	ptr = head;
 	while (i < index && ptr != NULL)
 	{
