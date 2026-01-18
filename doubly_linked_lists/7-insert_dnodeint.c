077a3cfe4f2aacf70b1374f1a3aea9fe8b10aeb2
Your Name <you@example.com>
2026-01-18 17:10:22 +0000

    add

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - function
+ * @h:head
+ * @idx:index
+ * @n: number
+ * Return:the address of new node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *ptr, temp;
+	unsigned int i = 0;
+	
+	if (head == NULL || idx < 0)
+		return (NULL);
+	temp = malloc(sizeof(dlistint_t));
+	if (temp == NULL)
+		return (NULL);
+	temp->prev = NULL;
+	temp->n = n;
+	temp->next = NULL;
+	if (*head == NULL && idx == 0)
+	{
+		*head = temp;
+		return (temp)
+	}
+	ptr = *head
+	for ( i = 0; ptr != NULL && i < idx - 1; i++)
+	{
+        	ptr = ptr->next;
+	}
+	if (ptr == NULL)
+		return (NULL);
+	temp->next = ptr->next;
+	temp->prev = ptr;
+	return (temp);
+}
