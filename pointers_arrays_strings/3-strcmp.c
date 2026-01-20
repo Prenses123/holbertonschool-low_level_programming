341834464a5fe0df32b7d812ae77fda045e44658
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2026-01-20 19:53:39 +0400

    Update 3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -8,28 +8,21 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int len1, len2;
-	int i = 0, j = 0;
+	int len1;
+	int i = 0;
 
 	if (s1 == NULL || s2 == NULL)
 		return (-1);
 	while (s1[i] != '\0')
 		i++;
-	while (s2[j] != '\0')
-                j++;
+	//while (s2[j] != '\0')
+               // j++;
 	len1 = i;
-	len2 = j;
-	if (len1 != len2)
-	{
-		if (len1 > len2)
-			return (1);
-		else
-			return (-1);
-	}
+	//len2 = j;
 	for (i = 0; i < len1; i++)
 	{
 		if (s1[i] != s2[i])
-			return (1);
+			return (s1[i] - s2[i]);
 	}
 	return (0);
 }
