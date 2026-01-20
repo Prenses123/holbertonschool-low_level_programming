274c99a83f6dcc7a7654727d8e3403dd6f5ca50f
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2026-01-20 20:33:49 +0400

    Update 4-rev_array.c

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -8,17 +8,18 @@
  */
 void reverse_array(int *a, int n)
 {
-    int temp;
-    int i, j;
-    
-    i = 0;
-    j = n - 1;
-    while (i < j)
-    {
-        temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
-        i++;
-        j--;
-    }
+	int temp;
+	int i, j;
+
+	i = 0;
+	j = n - 1;
+
+	while (i < j)
+	{
+		temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+		i++;
+		j--;
+	}
 }
