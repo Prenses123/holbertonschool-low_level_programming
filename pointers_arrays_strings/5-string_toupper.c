67835f4f9a3c3034e2387ed5eae41d600d1c3456
Your Name <you@example.com>
2026-01-21 12:57:49 +0000

    add

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * string_toupper - function
+ * @str: string
+ * Return:upper
+ */
+char *string_toupper(char *str)
+{
+	int i =0;
+
+	while(str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+		i++;
+	}
+	return (str);
+}
