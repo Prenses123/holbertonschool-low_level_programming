5cb3b8104a8ace9f8a6d1606ebbf75e080d8c2cc
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2026-01-25 18:41:35 +0400

    Update 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
+#include "main.h"
 /**
- * main - Bir dosyanın içeriğini başka bir dosyaya kopyalar.
- * @argc: Argüman sayısı.
- * @argv: Argüman vektörü.
- *
- * Return: Başarı durumunda 0.
+ * main - Bir dosyayi baska bir dosyaya kopyalar.
+ * @argc: Arguman sayisi.
+ * @argv: Arguman vektoru.
+ * Return: 0.
  */
 int main(int argc, char *argv[])
 {
-	int f_from, f_to, r_status, w_status;
+	int f_from, f_to, r, w;
 	char buffer[1024];
 
 	if (argc != 3)
@@ -27,32 +27,24 @@ int main(int argc, char *argv[])
 	if (f_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(f_from);
+		close_fd(f_from);
 		exit(99);
 	}
-	while ((r_status = read(f_from, buffer, 1024)) > 0)
+	while ((r = read(f_from, buffer, 1024)) > 0)
 	{
-		w_status = write(f_to, buffer, r_status);
-		if (w_status == -1 || w_status != r_status)
+		w = write(f_to, buffer, r);
+		if (w == -1 || w != r)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
-	if (r_status == -1)
+	if (r == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (close(f_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_from);
-		exit(100);
-	}
-	if (close(f_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_to);
-		exit(100);
-	}
+	close_fd(f_from);
+	close_fd(f_to);
 	return (0);
 }
