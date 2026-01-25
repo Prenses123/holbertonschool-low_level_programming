0c97c875e714065abcfd503454a298b486894994
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2026-01-25 18:42:12 +0400

    Update main.h

--- file_io/main.h
+++ file_io/main.h
@@ -5,7 +5,20 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+/**
+ * close_fd - Dosya tanimlayicisini kapatir ve hata kontrolu yapar.
+ * @fd: Kapatilacak dosya tanimlayicisi.
+ */
+void close_fd(int fd);
 
+void close_fd(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
