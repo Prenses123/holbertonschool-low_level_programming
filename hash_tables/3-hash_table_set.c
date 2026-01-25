3e429ade5198b0c09470fa6b0d0ea60c8229fc87
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2026-01-25 19:14:16 +0400

    Update 3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -3,56 +3,38 @@
 /**
  * hash_table_set - Hash tablosuna eleman ekler veya gunceller.
  * @ht: Hash tablosu.
- * @key: Anahtar (bos olamaz).
- * @value: Deger (kopyalanmalidir).
- *
+ * @key: Anahtar.
+ * @value: Deger.
  * Return: Basari durumunda 1, aksi halde 0.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node, *temp;
-	char *new_value;
 	unsigned long int index;
+	hash_node_t *new_node, *temp;
+	char *v_copy;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
-	new_value = strdup(value);
-	if (new_value == NULL)
-		return (0);
-
 	index = key_index((const unsigned char *)key, ht->size);
 	temp = ht->array[index];
-
 	while (temp)
 	{
 		if (strcmp(temp->key, key) == 0)
 		{
+			v_copy = strdup(value);
+			if (v_copy == NULL)
+				return (0);
 			free(temp->value);
-			temp->value = new_value;
+			temp->value = v_copy;
 			return (1);
 		}
 		temp = temp->next;
 	}
-
-	new_node = malloc(sizeof(hash_node_t));
+	new_node = create_node(key, value);
 	if (new_node == NULL)
-	{
-		free(new_value);
 		return (0);
-	}
-
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_value);
-		free(new_node);
-		return (0);
-	}
-
-	new_node->value = new_value;
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-
 	return (1);
 }
