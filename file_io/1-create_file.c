6666a71a565f3d2865ed8d0c0d027c534cf1d42a
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2026-01-25 17:52:39 +0400

    Update 1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -4,11 +4,12 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	int n_written;
 	int l = 0;
+
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
-		return(-1);
+		return (-1);
 	if (text_content != NULL)
 	{
 		while (text_content[l])
@@ -17,9 +18,9 @@ int create_file(const char *filename, char *text_content)
 		if (n_written == -1 || n_written != l)
 		{
 			close(fd);
-			return(-1);
+			return (-1);
 		}
 	}
 	close(fd);
-	return(1);
+	return (1);
 }
