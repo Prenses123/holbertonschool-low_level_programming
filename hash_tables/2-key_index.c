b4d0cf70e0666aa773aad3737c68d39c160983d7
root <root@ip-10-42-56-172.ec2.internal>
2026-01-25 15:07:19 +0000

    add

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Bir anahtarin saklanacagi indeksi dondurur.
+ * @key: Anahtar (string).
+ * @size: Hash tablosunun dizi boyutu.
+ *
+ * Return: Anahtarin indeksi.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash;
+
+	if (key == NULL || size == 0)
+		return (0);
+	hash = hash_djb2(key);
+	return (hash % size);
+}
