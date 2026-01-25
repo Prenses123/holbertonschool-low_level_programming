deccdca556f04f9b5430c527ff310ac85f550730
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2026-01-25 19:16:38 +0400

    Update 3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,40 +1,51 @@
+#define _GNU_SOURCE
 #include "hash_tables.h"
-#include <string.h>
+
 /**
- * hash_table_set - Hash tablosuna eleman ekler veya gunceller.
- * @ht: Hash tablosu.
- * @key: Anahtar.
- * @value: Deger.
- * Return: Basari durumunda 1, aksi halde 0.
+ * hash_table_set - Adds or updates an element in the hash table.
+ * @ht: The hash table.
+ * @key: The key.
+ * @value: The value.
+ * Return: 1 on success, 0 otherwise.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *new_node, *temp;
+	unsigned long int idx;
+	hash_node_t *new, *tmp;
 	char *v_copy;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+	if (!ht || !key || !*key || !value)
 		return (0);
-
-	index = key_index((const unsigned char *)key, ht->size);
-	temp = ht->array[index];
-	while (temp)
+	v_copy = strdup(value);
+	if (!v_copy)
+		return (0);
+	idx = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[idx];
+	while (tmp)
 	{
-		if (strcmp(temp->key, key) == 0)
+		if (strcmp(tmp->key, key) == 0)
 		{
-			v_copy = strdup(value);
-			if (v_copy == NULL)
-				return (0);
-			free(temp->value);
-			temp->value = v_copy;
+			free(tmp->value);
+			tmp->value = v_copy;
 			return (1);
 		}
-		temp = temp->next;
+		tmp = tmp->next;
 	}
-	new_node = create_node(key, value);
-	if (new_node == NULL)
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
+	{
+		free(v_copy);
 		return (0);
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
+	}
+	new->key = strdup(key);
+	if (!new->key)
+	{
+		free(v_copy);
+		free(new);
+		return (0);
+	}
+	new->value = v_copy;
+	new->next = ht->array[idx];
+	ht->array[idx] = new;
 	return (1);
 }
