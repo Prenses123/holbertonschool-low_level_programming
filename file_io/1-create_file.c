b5dc997c1d02f025b443b9c362830c4e627b19b2
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2026-01-25 17:54:05 +0400

    Update 1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+* create_file - function
+* @filename: name of file
+* @text_content: file
+* Return:1
+*/
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
